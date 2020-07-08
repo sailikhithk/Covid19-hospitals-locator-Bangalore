5c05fce82337c05061994349204219acbf601826
sailikhith7 <likki1996@gmail.com>
2020-07-08 09:29:33 -0400

    Updated the app

---
+++ js/covid-hospitals-data.js
@@ -0,0 +1,285 @@
+let hospitals = [{
+    "name": "Jayanagar General Hospital",
+    "phoneNumber":"080 2244 5711",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9262377,
+        "longitude": 77.5906121, 
+    },
+
+    "address":"32nd Cross, 4th B Block, beside Rajiv Gandhi Health Institute, Tilak Nagar, Jayanagar, Bengaluru, Karnataka 560041",
+    "addressLines":[
+        "32nd Cross, 4th B Block", 
+        "beside Rajiv Gandhi Health Institute", 
+        "Tilak Nagar, Jayanagar", 
+        "Bengaluru, Karnataka 560041"],
+    "pincode": "560041",
+    "zipCode":"56004", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "3.6",
+},
+
+{
+    "name": "Epidemic Disease Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9860325,
+        "longitude": 77.6420924,
+    },
+    "address":"Indira Nagar I Stage, Hoysala Nagar, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["Indira Nagar I Stage", "Hoysala Nagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", /* first few digits */
+    "numBeds" : "10",
+    "rating" : "3.4",
+},
+
+{
+    "name": "Bowring and Lady Curzon Hospital",
+    "phoneNumber":"080 2559 1325",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.982188,
+        "longitude": 77.6018685,
+    },
+    "address":"XJJ3+VJ Shivaji Nagar, Bengaluru, Karnataka 560001",
+    "addressLines":["XJJ3+VJ Shivaji Nagar", "Bengaluru, Karnataka"],
+    "pincode": "560001",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "2",
+    "rating" : "3.7",
+},
+
+
+{
+    "name": "KC General Hospital",
+    "phoneNumber":"080 2334 1771",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9961428,
+        "longitude": 77.5671693,
+    },
+    "address":"Ground, Malleswaram Circle, Police Station Rd, Bengaluru, Karnataka 560003",
+    "addressLines":["Ground, Malleswaram Circle", "Police Station Rd", "Bengaluru, Karnataka 560003"],
+    "pincode": "560003",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "1",
+    "rating" : "3.1",
+},
+
+{
+    "name": "SDS Tuberculosis Research Center & Rajiv Gandhi Institute of Chest Diseases",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.938365,
+        "longitude": 77.5885142,
+    },
+    "address":"Someshwaranagara Dharmaram College Post, 1st Main Rd, near NIMHANS, Bengaluru, Karnataka 560029",
+    "addressLines":["Someshwaranagara Dharmaram College Post", "1st Main Rd, near NIMHANS", "Bengaluru, Karnataka 560029"],
+    "pincode": "560029",
+    "zipCode":"56002", /* first few digits */
+    "numBeds" : "12",
+    "rating" : "3",
+},
+
+
+{
+    "name": "Manipal Hospitals",
+    "phoneNumber":"080 2502 4444",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9583911,
+        "longitude": 77.6466477,
+    },
+    "address":"98, HAL Old Airport Rd, Kodihalli, Bengaluru, Karnataka 560017",
+    "addressLines":["98, HAL Old Airport Rd", "Kodihalli", "Bengaluru, Karnataka 560017"],
+    "pincode": "560017",
+    "zipCode":"56001", /* first few digits */
+    "numBeds" : "15",
+    "rating" : "4.1",
+},
+
+
+{
+    "name": "Narayana Health City",
+    "phoneNumber":"080675 06870",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8277197,
+        "longitude": 77.6571877,
+    },
+    "address":"258/A, Bommasandra Industrial Area Anekal Taluk, Hosur Rd, Bengaluru, Karnataka 560099",
+    "addressLines":["258/A, Bommasandra Industrial Area Anekal Taluk", "Hosur Rd", "Bengaluru, Karnataka 560099"],
+    "pincode": "560099",
+    "zipCode":"56009", /* first few digits */
+    "numBeds" : "20",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Fortis Hospital",
+    "phoneNumber":"096633 67253",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8948227,
+        "longitude": 77.5964309,
+    },
+    "address":"154, 9, Bannerghatta Main Rd, Opposite IIM, Sahyadri Layout, Panduranga Nagar, Bengaluru, Karnataka 560076",
+    "addressLines":["154, 9, Bannerghatta Main Rd", "Opposite IIM, Sahyadri Layout", "Panduranga Nagar", "Bengaluru, Karnataka 560076"],
+    "pincode": "560076",
+    "zipCode":"56007", /* first few digits */
+    "numBeds" : "6",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Victoria Hospital",
+    "phoneNumber":"080 2670 1150",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9635669,
+        "longitude": 77.5716187,
+    },
+    "address":"Fort Rd, near City Market, New Tharagupet, Bengaluru, Karnataka 560002",
+    "addressLines":["Fort Rd, near City Market", "New Tharagupet", "Bengaluru, Karnataka 560002"],
+    "pincode": "560002",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "3.5",
+},
+
+
+{
+    "name": "Sir.C.V. Raman General Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9829148,
+        "longitude": 77.6428149,
+    },
+    "address":"68/1, 4th B Cross Rd, Kalyan Nagar, Michael Palaya, H Colony, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["68/1, 4th B Cross Rd", "Kalyan Nagar, Michael Palaya", "H Colony, Indiranagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "3.8",
+},
+
+{
+    "name": "ESIC Model Hospital",
+    "phoneNumber":"+918023325130",
+    "openTime" : "9am-4pm",
+    "coordinates": {
+        "latitude": 12.9916924,
+        "longitude": 77.551712,
+    },
+    "address":"620/A/1, 12th Main Rd, 2nd Block, Rajajinagar, Bengaluru, Karnataka 560010",
+    "addressLines":["620/A/1, 12th Main Rd", "2nd Block, Rajajinagar", "Bengaluru, Karnataka 560010"],
+    "pincode": "560010",
+    "zipCode":"56001", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "2.9",
+},
+
+{
+    "name": "General Hospital Devanahalli",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.2568568,
+        "longitude": 77.7252711,
+    },
+    "address":"SH 96, Devanahalli, Binnamangala, Karnataka 562110",
+    "addressLines":["SH 96, Devanahalli", "Binnamangala, Karnataka 562110"],
+    "pincode": "562110",
+    "zipCode":"56211", /* first few digits */
+    "numBeds" : "4",
+    "rating" : "2.8",
+},
+
+{
+    "name": "BGS Global Institute of Medical Sciences",
+    "phoneNumber":"+918026984802",
+    "openTime" : "Temporarily closed",
+    "coordinates": {
+        "latitude": 12.9022857,
+        "longitude": 77.4956326,
+    },
+    "address":"No. 67, BGS Health & Education City, Uttarahalli Road, Kengeri, Bengaluru, Karnataka 560060",
+    "addressLines":["No. 67, BGS Health & Education City", "Uttarahalli Road, Kengeri", "Bengaluru, Karnataka 560060"],
+    "pincode": "560060",
+    "zipCode":"56006", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "4.1",
+},
+
+{
+    "name": "Rajarajeshwari Medical College and Hospital",
+    "phoneNumber":"+918028437444",
+    "openTime" : "Temporatily closed",
+    "coordinates": {
+        "latitude": 12.8961554,
+        "longitude": 77.4596629,
+    },
+    "address":"202, Mysore Road Kambipura, Bengaluru, Karnataka 560074",
+    "addressLines":["202, Mysore Road Kambipura", "Bengaluru, Karnataka 560074"],
+    "pincode": "560074",
+    "zipCode":"56007", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "3.6",
+},
+
+{
+    "name": "General Hospital,Nelamangala",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.0987354,
+        "longitude": 77.389022,
+    },
+    "address":"Jayanagara, Indiranagara, Karnataka 562123",
+    "addressLines":["Jayanagara, Indiranagara", "Karnataka 562123"],
+    "pincode": "562123",
+    "zipCode":"56212", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "2.8",
+},
+
+{
+    "name": "General Hospital, Doddaballapur",
+    "phoneNumber":"Not Available",
+    "openTime" : "Not Available",
+    "coordinates": {
+        "latitude": 13.2843621,
+        "longitude": 77.5292349,
+    },
+    "address":"7GVV+2J Doddaballapura, Karnataka 561203",
+    "addressLines":["7GVV+2J Doddaballapura", "Karnataka 561203"],
+    "pincode": "561203",
+    "zipCode":"56120", /* first few digits */
+    "numBeds" : "4",
+    "rating" : "4.2",
+},
+
+{
+    "name": "General Hospital, Anekal",
+    "phoneNumber":"Not Available",
+    "openTime" : "9am-4:30pm",
+    "coordinates": {
+        "latitude": 12.7575095,
+        "longitude": 77.6367502,
+    },
+    "address":"Weaver Colony, Anekal, Karnataka 562106",
+    "addressLines":["Weaver Colony, Anekal", "Karnataka 562106"],
+    "pincode": "562106",
+    "zipCode":"56210", /* first few digits */
+    "numBeds" : "6",
+    "rating" : "4.1",
+},
+]
+
+
---
+++ js/index.js
@@ -0,0 +1,302 @@
+
+ var map;
+ var markers = [];
+ var infoWindow;
+ var currLatitude;
+ var currLongitude;
+
+ function initMap() {
+    var Bangalore = {
+        lat: 12.9507432,
+        lng: 77.5825886,
+    }
+     map = new google.maps.Map(document.getElementById('map'), {
+     center: Bangalore,
+     zoom: 15,
+     styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+     ]
+     });
+
+     /* Initialise the Info Window */
+     infoWindow = new google.maps.InfoWindow();
+
+     /* Initialize the Hospital list */
+     searchHospitals();
+     setOnClickListener();
+     getLocation();
+ }
+
+ function displayhospitals (hospitals) {
+     var hospitalsList ="";
+     var address;
+     var phone;
+    hospitals.forEach(function(hospital, index) {
+        address = hospital.addressLines;
+        phone = hospital.phoneNumber;
+        numBed = hospitals.numBeds;
+        hospitalsList += `
+        <div class="hospital-container">
+            <div class="hospital-container-background">
+                <div class="hospital-info-container"> 
+                    <div class="hospital-address">
+                        <span>${address[0]}</span> 
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="hospital-phone-number">Ph: ${phone}</div>
+                </div>
+                <div class="hospital-number-container">
+                    <div class="hospital-number">${index+1}</div>
+                </div>
+            </div>
+        </div>
+        `
+    });
+    document.querySelector('.hospitals-list').innerHTML = hospitalsList;
+   }
+
+   /* Show hospitals pin markers on map */
+   function showhospitalsMarker(hospitals, search) {
+    var latlng;
+    var name;
+    var address;
+    var statusText;
+    var phoneNumber;
+    var numBed;
+    var bounds = new google.maps.LatLngBounds();
+    hospitals.forEach(function(hospital, index) {
+    latlng = new google.maps.LatLng(
+        hospital.coordinates.latitude,
+        hospital.coordinates.longitude);
+    name = hospital.name;
+    address = hospital.addressLines[0];
+    statusText = hospital.openTime;
+    phoneNumber = hospital.phoneNumber;
+    numBed = hospital.numBeds;
+    var rating = hospital.rating;
+    
+    createMarker(latlng, name, address, statusText, phoneNumber, numBed, index, rating);
+    setMarkerDirections(hospital.coordinates.latitude, hospital.coordinates.longitude, index);
+    setMarkerAnimations(markers[index], search)
+    bounds.extend(latlng);
+   });
+   map.fitBounds(bounds);
+}
+
+function setMarkerAnimations (marker, search) {
+    if (search == true)
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    else
+        marker.setAnimation(google.maps.Animation.DROP);
+}
+
+function createMarker(latlng, name, address, statusText, phoneNumber, numBed, index, rating) {
+var html = `
+    <div class="hospital-info-window">
+        <div class="hospital-info-name">
+            ${name} &nbsp ${rating}<span class="fa fa-star checked fa-xs"></span>
+        </div>
+        <div class="hospital-info-status">
+            ${statusText}
+        </div>
+        <div id="hospital-address-id" class="hospital-info-address">
+            <div class="circle">
+                <i class="fas fa-location-arrow"></i>
+            </div>
+            ${address}
+        </div>
+        <div class="hospital-info-phone">
+            <div class="circle">
+                <i class="fas fa-phone-alt"></i>
+            </div>
+            ${phoneNumber}
+        </div>
+        <div class="hospital-info-beds">
+            <div class="circle">
+                <i class="fas fa-procedures"></i>
+            </div>
+            ${numBed}
+        </div>
+    </div>
+    `
+var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: `${index+1}`,
+});
+
+google.maps.event.addListener(marker, 'mouseover', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+});
+markers.push(marker);
+}
+
+
+
+function setMarkerDirections(lat, long, index) {
+google.maps.event.addListener(markers[index], 'click', function() {
+    setOnClickListenerAddress(lat, long);
+});
+}
+
+/* Open the info window marker on hospital selection in hospitals list */
+function setOnClickListener() {
+    var hospitalElements = document.querySelectorAll('.hospital-container');
+    hospitalElements.forEach(function(elem, index) {
+        elem.addEventListener('click', function() {
+            google.maps.event.trigger(markers[index], 'mouseover');
+        })
+    })
+}
+
+function clearLocations(){
+infoWindow.close();
+for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+}
+markers.length = 0; /* Find out why */
+}
+
+/* Allow a user to search for the hospitals using zip code */
+function searchHospitals() {
+    var search = false;
+    var zipCode = document.getElementById('zip-code-input').value;
+    if (zipCode) {
+    var foundHospitals = [];
+    hospitals.forEach(function(hospital){
+        var postal = hospital.zipCode.substring(0,5);
+        if (zipCode == hospital.pincode || zipCode == postal) {
+            foundHospitals.push(hospital);
+            /* FIXME zoom-out a little bit when results are few, it gets too zoomed */
+        } else {
+            /* FIXME:Handle this */
+        }
+    });
+    search = true;
+    clearLocations();
+} else {
+    foundHospitals = hospitals; 
+}
+displayhospitals(foundHospitals);
+showhospitalsMarker(foundHospitals, search);
+}
+
+function saveCurrentLocation(position) {
+    currLatitude = position.coords.latitude;
+    currLongitude = position.coords.longitude;
+    console.log(currLatitude, currLongitude);
+ }
+
+function errorHandler(err) {
+    if(err.code == 1) {
+       alert("Error: Access is denied!");
+    } else if( err.code == 2) {
+       alert("Error: Position is unavailable!");
+    }
+ }
+
+function getLocation(){
+    if(navigator.geolocation){
+       // timeout at 60000 milliseconds (60 seconds)
+       var options = {timeout:60000};
+       navigator.geolocation.getCurrentPosition
+       (saveCurrentLocation, errorHandler, options);
+    } else{
+       alert("Sorry, browser does not support geolocation!");
+    }
+ }
+
+function setOnClickListenerAddress(lat, lng) {
+    
+            var url = "https://www.google.com/maps/dir/?api=1";
+            var origin = "&origin=" + currLatitude + "," + currLongitude;
+            var destination = "&destination=" + lat + "," + lng;
+            var newUrl = new URL(url + origin + destination);
+            console.log(newUrl);
+            window.open(newUrl, '_blank');
+
+            
+}
+
+
+
+/* experimental code here */
+
